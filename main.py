a00cb56319562d2b2f8fd0169da48bbabaa8651e
LOUDINI SOUAD <souad.llddnn@gmail.com>
2023-12-01 14:27:56 +0100

    added script to extract ethereum latest block number

---
+++ main.py
@@ -0,0 +1,28 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+ALCHEMY_KEY = os.getenv("ALCHEMY_KEY")
+url = f"https://eth-mainnet.g.alchemy.com/v2/{ALCHEMY_KEY}"
+
+headers = {
+    "Content-Type": "application/json",
+}
+
+data = {
+    "jsonrpc": "2.0",
+    "method": "eth_blockNumber",
+    "params": [],
+    "id": 0,
+}
+
+response = requests.post(url, headers=headers, json=data)
+
+if response.status_code == 200:
+    print(response.json())
+else:
+    print(f"Request failed with status code {response.status_code}")
+    print(response.text)
