e69a430b356be1f123b5d0441f26a1f367b60d3b
LOUDINI SOUAD <souad.llddnn@gmail.com>
2024-02-01 14:56:31 +0100

    train model file

--- train.py
+++ train.py
@@ -3,25 +3,30 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.preprocessing import StandardScaler
+from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import make_pipeline
 
-
-
 data = pd.read_csv('data.csv')
+
 y = data["contract_type"] == "valid"
 X = data["padded_arrays"]
 
-
-
-#print("ooooooooooooooo")
-#print(X.apply(lambda s: len(json.loads(s))))
-
-
+# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-model = make_pipeline(StandardScaler(), LogisticRegression())
 
+# Define the preprocessing pipeline
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('padded_arrays', make_pipeline(eval, StandardScaler()), ['padded_arrays'])
+    ])
+
+# Create the final pipeline with the logistic regression model
+model = make_pipeline(preprocessor, LogisticRegression())
+"""
+# Train the model
 model.fit(X_train, y_train)
 
+# Make predictions on the test set
 y_pred = model.predict(X_test)
 
 # Evaluate the performance of the model
@@ -34,4 +39,5 @@ print(f"Accuracy: {accuracy:.4f}")
 print("\nConfusion Matrix:")
 print(conf_matrix)
 print("\nClassification Report:")
-print(classification_rep) 
+print(classification_rep)
+"""
