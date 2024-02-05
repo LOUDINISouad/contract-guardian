5a1b31af2985cc69448f5bc37a414b1a3c8a6624
LOUDINI SOUAD <souad.llddnn@gmail.com>
2024-02-05 12:12:50 +0100

    Update the main file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
-
 import requests
 from dotenv import load_dotenv
-
+from preprocess import get_raw_dataset, expand_array
+from train import train_model
 
 load_dotenv()
 
@@ -27,3 +27,18 @@ if response.status_code == 200:
 else:
     print(f"Request failed with status code {response.status_code}")
     print(response.text)
+
+# Preprocess the raw_data.csv file
+try:
+    raw_df = get_raw_dataset()
+    expanded_df = expand_array(raw_df)
+    expanded_df.to_csv("data.csv", index=False)
+   
+except Exception as e:
+    print(f"An error occurred during preprocessing: {e}")
+
+# Train the model
+try:
+    train_model()
+except Exception as e:
+    print(f"An error occurred during model training: {e}")
